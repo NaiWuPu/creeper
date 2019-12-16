5a52e94a33a775be48d00c654007a294899d8e3a
lvxin <lvxin0315@163.com>
2019-12-16 17:55:46 +0800

    生成32位

--- test/app_secret_test.go
+++ test/app_secret_test.go
@@ -13,10 +13,16 @@ func Test_secret(t *testing.T) {
 	var secrets []string
 	for i := 0; i <= 10000; i++ {
 		newSecrets := app.UniqueId()
+		if len(newSecrets) == 32 {
+			t.Error("长度是：", len(newSecrets))
+			t.Fail()
+			return
+		}
 		//判断是否有重复的
 		for _, s := range secrets {
 			if newSecrets == s {
 				t.Error("有重复")
+				t.Fail()
 				return
 			}
 		}
--- test/encryption_aes_test.go
+++ test/encryption_aes_test.go
@@ -16,6 +16,7 @@ func Test_encrypt(t *testing.T) {
 	//解密
 	if encryption.AesDecrypt(encryptOrig, encryptionAesTestKey) != encryptionAesTestOrig {
 		t.Error("加密 != 解密")
+		t.Fail()
 		return
 	}
 }
