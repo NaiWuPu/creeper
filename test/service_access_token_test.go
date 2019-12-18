0845b4c102a7955886751375dbfac2a69310aae4
lvxin <lvxin0315@163.com>
2019-12-18 16:45:12 +0800

    验证access_token

--- test/service_access_token_test.go
+++ test/service_access_token_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Test_create_access_token(t *testing.T) {
-	ot := service.CreateAccessToken(1, "sss")
+	ot := service.CreateAccessToken(1, "24cd1a8fa6da73dc78e2f56619cc70de")
 	if ot.Error != nil {
 		t.Error(ot.Error)
 		t.Fail()
@@ -16,7 +16,7 @@ func Test_create_access_token(t *testing.T) {
 	}
 	log.Println(ot.Data.(*orm_model.AccessToken).Token)
 	//第二次创建
-	ot1 := service.CreateAccessToken(1, "sss")
+	ot1 := service.CreateAccessToken(1, "24cd1a8fa6da73dc78e2f56619cc70de")
 	if ot1.Error != nil {
 		t.Error(ot1.Error)
 		t.Fail()
